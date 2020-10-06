4b312496788ffa672cb199a8f28d899a784b82af
Emily Scott <62396942+escott2@users.noreply.github.com>
2020-10-06 11:21:18 -0500

    Collect order user input

--- index.js
+++ index.js
@@ -1,10 +1,53 @@
-const addItem = document.getElementById("vcc-qty");
-const addButton = document.querySelectorAll(".js-add-to-cart");
 const orderScreen = document.querySelector(".js-order-screen");
+let order = [];
 
-orderScreen.addEventListener("click", (event) => {
+const addProduct = (event) => {
+
     if (event.target.tagName == 'BUTTON') {
-        const prevSib = event.target.previousElementSibling;
-        console.log(prevSib.value);
+
+        const button = event.target;
+        const product = event.target.parentNode.parentNode.parentNode;
+        const getProductName = product.querySelector(".js-product-name").textContent;
+        const getQuantity = product.querySelector(".js-product-quantity").value;
+        const getPrice = product.querySelector(".js-unit-price").textContent;
+
+        event.preventDefault();
+
+        let productData = {
+            name: getProductName,
+            quantity: getQuantity,
+            price: getPrice,
+        }
+        order.push(productData);
+
+        button.disabled = true;
+
     }
-});
+}
+
+orderScreen.addEventListener("click", addProduct);
+
+console.log(order);
+
+
+
+
+
+// orderScreen.addEventListener("click", (event) => {
+
+//     event.preventDefault();
+//     if (event.target.tagName == 'BUTTON') {
+
+//         const product = event.target.parentNode.parentNode.parentNode;
+//         const getProductName = product.querySelector(".js-product-name").textContent;
+//         const getQuantity = product.querySelector(".js-product-quantity").value;
+//         const getPrice = product.querySelector(".js-unit-price").textContent;
+
+
+//         console.log(getProductName);
+//         console.log(getPrice);
+//         console.log(getQuantity);
+
+
+//     }
+// });

62ea6d533ac2aea67cd273314e5ef501a55e1ffd
Emily Scott <62396942+escott2@users.noreply.github.com>
2020-10-08 21:30:49 -0500

    Update content generation

--- JS/product.js
+++ JS/product.js
@@ -1,22 +1,80 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    const loadMenu = document.querySelector(".js-load-menu");
     var obj = JSON.parse(product);
+    // const donutObjects = Object.values(obj);
+    const donutObjects = obj.donuts;
+
     let titleContent = document.querySelector(".test");
-    let html = `<div class="col-sm">
-                    <div class="card">
-                        <div class="card-body">`;
+    let html = "";
+
+
+
+
+
+
+    const generateContent = (event) => {
+        if (event.target.tagName == 'BUTTON') {
+
+            for (let i = 0; i < donutObjects.length; i++) {
+                console.log(donutObjects[i]["name"]);
+                html += `
+                <div class="col-sm">
+                <div class="card">
+                <div class="card-body">
+                `;
+                html += `
+                    <h5 class="card-title">${donutObjects[i]["name"]}</h5>
     
-    html +=                 `<h5 class="card-title">${obj.donuts[0].name}</h5>`;
+                    </div>
+                    </div>
+                    </div>
+                    `;
+
+            }
+
+            titleContent.innerHTML = html;
 
 
-    html +=         `</div>
-                </div>
-            </div>`;
+            // for (let i = 0; i < donutObjects.length; i++) {
+            //     //enters first array
+            //     console.log("test");
+
+            //     for (let y = 0; y < donutObjects[i].length; y++) {
+            //         // enters objects within array
 
 
-    titleContent.innerHTML = html;
+            //         console.log(donutObjects[i][y].name);
+            // console.log(donutObjects);
 
-    console.log(obj);
+            // console.log(donutObjects[i].name);
+            // html += `
+            //     <h5 class="card-title">${product[i]["name"]}</h5>
+
+            //     </div>
+            //     </div>
+            //     </div>`;
+
+            // titleContent.innerHTML = html;
+
+            //     }
+            // }
+        }
+    }
+
+
+
+
+
+    console.log(donutObjects);
+
+
+    // console.log(obj);
+    // console.log(donutObjects);
+    // console.log(html)
+
+
+    loadMenu.addEventListener("click", generateContent);
+
 
 })
-
